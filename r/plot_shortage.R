0deb0c53ae4212cfa835e9364cdf94a05615424f
David Love <dlove@math.arizona.edu>
2013-11-04 16:03:28 -0500

    Generate plots of shortages for both piecewise and linear fit 	new file:   images/linear_shortage_frequency.pdf 	renamed:    images/shortage_frequency.pdf -> images/piecewise_shortage_frequency.pdf 	new file:   r/linear_shortage_pworst.csv 	new file:   r/piecewise_shortage_pworst.csv 	deleted:    r/shortages.csv

--- r/plot_shortage.R
+++ r/plot_shortage.R
@@ -2,11 +2,20 @@ require(ggplot2)
 
 source('plot_options.R')
 
-shortage <- read.csv('shortages.csv')
+shortage <- read.csv('piecewise_shortage_pworst.csv')
 shortage$Infrastructure <- ordered(shortage$Infrastructure, c('None','WWTP','IPR','RO'))
 p <- qplot(x=Shortage, data=shortage, facets=~Infrastructure, fill=Population) + 
   xlab('Total Shortage (af)') +
   ylab('Count') +
   my.theme
 
-ggsave(paste(folder, 'shortage_frequency', extension, sep=''), plot=p, width=width, height=0.67*width)
+ggsave(paste(folder, 'piecewise_shortage_frequency', extension, sep=''), plot=p, width=width, height=0.67*width)
+
+shortage <- read.csv('linear_shortage_pworst.csv')
+shortage$Infrastructure <- ordered(shortage$Infrastructure, c('None','WWTP','IPR','RO'))
+p <- qplot(x=Shortage, data=shortage, facets=~Infrastructure, fill=Population) + 
+  xlab('Total Shortage (af)') +
+  ylab('Count') +
+  my.theme
+
+ggsave(paste(folder, 'linear_shortage_frequency', extension, sep=''), plot=p, width=width, height=0.67*width)
