1a455a627289804ccb7f31e5df8649ae2f547e72
David Love <dlove@math.arizona.edu>
2013-11-10 11:33:03 -0700

    Update all results to using both projections simultaneously
    	new file:   images/combined_shortage_frequency.pdf

--- r/aggregate_probabilities.R
+++ r/aggregate_probabilities.R
@@ -1,20 +1,24 @@
 # Aggregate some probability results from the computations
 
-p <- read.csv('piecewise_shortage_pworst.csv')
+require(reshape2)
+
+p <- read.csv('combined_shortage_pworst.csv')
 
 p <- within(p,
             {
-              Climate.Model <- factor(sub(pattern='\\.1\\.rcp..$',
+              Climate.Model <- factor(sub(pattern='\\.1\\.rcp[2468][056].*$',
                                           replacement='' ,p$Model))
               Emissions <- factor(sub(pattern='.*?(rcp..?).*$',
                                       replacement='\\1' ,p$Model))
+              Fit <- factor(sub(pattern='.*\\.rcp[2468][056]\\.', replacement='', p$Model))
             }
            )
-levels(p$Climate.Model) <- list('CSIRO mk3.6'='csiro.mk3.6.0', 'GFDL-CM3'='gfdl.cm3',
+levels(p$Climate.Model) <- list('CSIRO'='csiro.mk3.6.0', 'GFDL-CM3'='gfdl.cm3',
                                 'GFDL-ESM2M'='gfdl.esm2m', 'HadGEM2-ES'='hadgem2.es',
                                 'MIROC5'='miroc5', 'MIROC-ESM-CHEM'='miroc.esm.chem')
 levels(p$Emissions) <- list('RCP2.6'='rcp26', 'RCP4.5'='rcp45',
                             'RCP6.0'='rcp60', 'RCP8.5'='rcp85')
+levels(p$Fit) <- list('Piecewise'='piecewise', 'Linear','linear')
 
 # Ensure they sum to exactly 1
 sum.p <- aggregate(pworst.kl ~ Infrastructure, data=p, FUN=sum)
@@ -26,7 +30,7 @@ for(ii in sum.p$Infrastructure)
 rm(sum.p)
 
 long.pmodel <- aggregate(pworst.kl ~ Infrastructure + Climate.Model + Emissions, data=p, FUN=sum)
-pmodel <- dcast(long.pmodel[long.pmodel$Infrastructure=='IPR', ],
+pmodel <- dcast(long.pmodel[long.pmodel$Infrastructure=='None', ],
                 Climate.Model ~ Emissions, value.var='pworst.kl',
                 margins=TRUE, fun.aggregate=sum)
 pmodel[,-1] <- round(pmodel[,-1], 4)
@@ -37,7 +41,7 @@ write.table(pmodel, file='../tables/pworst_model_kl.tex',
             sep=' & ', eol=' \\\\\n')
 
 long.smodel <- aggregate(Shortage ~ Infrastructure + Climate.Model + Emissions, data=p, FUN=max)
-smodel <- dcast(long.smodel[long.pmodel$Infrastructure=='IPR', ],
+smodel <- dcast(long.smodel[long.pmodel$Infrastructure=='None', ],
                 Climate.Model ~ Emissions, value.var='Shortage')
 names(smodel)[1] <- ''
 print(smodel)
--- r/plot_shortage.R
+++ r/plot_shortage.R
@@ -7,6 +7,7 @@ shortage$Infrastructure <- ordered(shortage$Infrastructure, c('None','WWTP','IPR
 p <- qplot(x=Shortage, data=shortage, facets=~Infrastructure, fill=Population) + 
   xlab('Total Shortage (af)') +
   ylab('Count') +
+  scale_fill_brewer(palette="Set3") +
   my.theme
 
 ggsave(paste(folder, 'piecewise_shortage_frequency', extension, sep=''), plot=p, width=width, height=0.67*width)
@@ -16,6 +17,21 @@ shortage$Infrastructure <- ordered(shortage$Infrastructure, c('None','WWTP','IPR
 p <- qplot(x=Shortage, data=shortage, facets=~Infrastructure, fill=Population) + 
   xlab('Total Shortage (af)') +
   ylab('Count') +
+  scale_fill_brewer(palette="Set3") +
   my.theme
 
 ggsave(paste(folder, 'linear_shortage_frequency', extension, sep=''), plot=p, width=width, height=0.67*width)
+
+
+shortage <- read.csv('combined_shortage_pworst.csv')
+shortage$Infrastructure <- ordered(shortage$Infrastructure, c('None','WWTP','IPR','RO'))
+shortage <- within(shortage,
+                   {Type=factor(paste(Population, ' pop, ', gsub('^.*\\.','', Model), ' fit', sep=''))})
+p <- qplot(x=Shortage, data=shortage, facets=~Infrastructure, fill=Type) + 
+  xlab('Total Shortage (af)') +
+  ylab('Count') +
+  scale_fill_brewer(palette="Set3") +
+  my.theme
+
+ggsave(paste(folder, 'combined_shortage_frequency', extension, sep=''), plot=p, width=width, height=0.5*width)
+  
